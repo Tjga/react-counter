828fb09be26cbfdb95cc41938ca5145c88abff6d
Gerardo Ruiz Arroyo <tiga.ruiz@gmail.com>
2020-05-15 12:33:59 -0700

    I added an age thing and changed the app

---
+++ src/components/NavItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NavItem(props) {
+    return(
+        <h1>hello, {props.name}</h1>
+    )
+}
+
+export default NavItem;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>hui</h1>
+                <p>bruh this aint legal</p>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
