8a53e02f33128ac3e0ae810f01318fe0ddd1b51f
Gerardo Ruiz Arroyo <tiga.ruiz@gmail.com>
2020-05-19 20:20:59 -0700

    yeah this fucking works like a charm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React from 'react';
-import Navbar from './components/Navbar'
-import NavItem from './components/NavItem'
+import "./index.css"
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-        <Navbar />
-        <NavItem name='jerry' age="13"/>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.doubleClick = this.doubleClick.bind(this)
+    this.clearClick = this.clearClick.bind(this)  
+  }
+  handleClick() {
+    console.log("ADDED 1")
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+  doubleClick() {
+    console.log("DOUBLED")
+    this.setState(prevState => {
+      return {
+        count: prevState.count + prevState.count
+      }
+    })
+  }
+  clearClick() {
+    this.setState(prevState => {
+      return {
+        count: 0
+      }
+    })
+  }
+  render() {
+    return(
+      <div className="app">
+        <div className="counter">
+        <h1>{this.state.count}</h1>
+        </div>
+        <div className="buttons">
+        <button onClick={this.handleClick}>+1</button>
+        <button onClick={this.doubleClick}>2x</button>
+        </div>
+      </div>
+    )
+  }
+
 }
-
 export default App;
